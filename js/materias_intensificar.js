f684ff31ae5cec209fce4b7cebd3679450ff98b2
Giana <113945161+giana2022@users.noreply.github.com>
2023-12-06 16:13:51 -0300

    mejore el algoritmo de la consulta por alumno

--- js/materias_intensificar.js
+++ js/materias_intensificar.js
@@ -4,17 +4,13 @@ let boton_consulta = document.getElementById('consulta-notas-bton');
 
 boton_consulta.addEventListener("click", ()=>{
     let id_estudiante = prompt('Ingrese el Id del alumno a consultar:')
-    fetch(url)
+    fetch(url+`/${id_estudiante}`)
     .then(respuesta => respuesta.json())
-    .then(datos_alumnos=>{
-        for(estudiante of datos_alumnos){
-            if (estudiante.id == id_estudiante){
+    .then(estudiante=>{
                 for(dato in estudiante){
                     if (estudiante[dato] < 7 && typeof(estudiante[dato] == 'number')&& dato!=='curso' && dato!=='id'){
                         alert(`El alumno: ${estudiante.nombre} debe intensificar ${dato}, su nota es ${estudiante[dato]}`)
                     }
                 }
-            }
-        }
-    })
-});
+            })
+        })
